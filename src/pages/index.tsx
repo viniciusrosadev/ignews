bbd8eb5735a08f56a5aa7a05de2c8b451abe43cd
Vinicius Rosa <viniciusrosa3525@gmail.com>
2021-04-12 19:56:27 -0300

    Global and module scope

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Head>
+        <title>Início</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
   )
 }
