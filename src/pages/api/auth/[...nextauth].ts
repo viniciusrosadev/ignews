6b2e536a7e5678f707b27df76eef0e67804a5fbb
Vinicius Rosa <viniciusrosa3525@gmail.com>
2021-04-20 10:51:49 -0300

    Create user and query faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -8,5 +11,31 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             scope: 'read:user'
         })
-    ]
+    ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(q.Collection('users'), { data: { email } }),
+                        q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+                    )
+                )
+                // await fauna.query(q.Create(q.Collection('users'), { data: email }))
+                return true
+            } catch {
+                return false
+            }
+        }
+    }
 })
